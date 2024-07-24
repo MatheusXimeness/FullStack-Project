9e5d8cbeaa972c6029fed5e218f759d6f1614f2d
MatheusXimeness <matheus.ximenes2016@outlook.com>
2024-07-24 17:32:40 -0300

    FEAT: Creating a domain class and a list of objects for it

--- back-end/src/main/java/com/matheusximenes/backend/domain/Categoria.java
+++ back-end/src/main/java/com/matheusximenes/backend/domain/Categoria.java
@@ -1,2 +1,19 @@
-package com.matheusximenes.backend.domain;public class Categoria {
+package com.matheusximenes.backend.domain;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Data
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    private String nome;
+
 }
--- back-end/src/main/java/com/matheusximenes/backend/resources/CategoriaResource.java
+++ back-end/src/main/java/com/matheusximenes/backend/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.matheusximenes.backend.resources;
 
+import com.matheusximenes.backend.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Rest está funcionando";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> categorias = new ArrayList<Categoria>();
+        categorias.add(cat1);
+        categorias.add(cat2);
+
+        return  categorias;
     }
 }
